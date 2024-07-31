0575776c99aa03ca1e657a7af46c69a1adb7e6df
danh vo <danhvohoai2@gmail.com>
2024-07-31 10:58:12 +0700

    Update

--- networks/inception.py
+++ networks/inception.py
@@ -3,6 +3,7 @@ import torch
 import torch.nn as nn
 from torchvision import models
 from torchvision.models import Inception_V3_Weights
+from networks.local_grad import gradient_filter
 #import torch.nn.functional as F
 
 
@@ -20,12 +21,14 @@ class Clf_model(nn.Module):
     
     def __init__(self, original_model, num_classes=1):
         super(Clf_model, self).__init__()
+        self.gradient_filter = gradient_filter
         input_dim = 768
         self.features = nn.Sequential(*list(original_model.children())[:-7])
         self.avgpool = nn.AdaptiveAvgPool2d((1, 1))
         self.head = MLPHead(input_dim=input_dim, output_dim=num_classes)
         
     def forward(self,x):
+        x = self.gradient_filter(x)
         x = self.features(x)
         x = self.avgpool(x)
         x = x.view(x.size(0), -1)
@@ -38,10 +41,11 @@ def inception_local_grad(pretrained=False, **kwargs):
     Args:
         pretrained (bool): If True, returns a model pre-trained on ImageNet
     """
-    inception_model = models.inception_v3(weights=None)
     if pretrained:
         inception_model = models.inception_v3(weights=Inception_V3_Weights.IMAGENET1K_V1)
-    
+    else:
+        inception_model = models.inception_v3(weights=None)
+
     model = Clf_model(inception_model,**kwargs)
         
     return model
